d9ecbe9890fa004d40a7913a5865a410a86a16da
SELVA GANESH G <119717606+Gans19@users.noreply.github.com>
2023-02-12 15:56:36 +0530

    Resume added in link

--- src/components/topcontainer/topcontent/topcontent.js
+++ src/components/topcontainer/topcontent/topcontent.js
@@ -9,7 +9,7 @@ const Topcontent = () => {
 			<div className="topcontent_container">
 				<h1>Mr. Selva Ganesh G</h1>
 				<p>A Professional Web Developer</p>
-				<a href="google.com">
+				<a href="https://docs.google.com/document/d/1YnBITxEhgLZfy-MmnRi6EpBPRLGpkqkivNrkXeq3LBI/edit?usp=share_link">
 					<button className="topcontent_downloadbtn">Download CV</button>
 				</a>
 				<Link to="project" smooth={true} duration={500}>
